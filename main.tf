8c90decfcea3d673796312e8af8465dc0d5fb520
pdelolmo1 <167941963+pdelolmo1@users.noreply.github.com>
2025-04-07 14:45:55 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,32 +1,21 @@
-module "tagging" {
-  source       = "./modules/tagging"
-  default_tags = var.default_tags
+terraform {
+  cloud {
+    organization = "PabloTerraformDemo"
+
+    workspaces {
+      name = "FinOps-Demo"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 }
 
-module "s3_cost_reports" {
-  source       = "./modules/s3_cost_reports"
-  default_tags = var.default_tags
+provider "aws" {
+  region = var.region
 }
 
-module "budget" {
-  source        = "./modules/budget"
-  email_address = var.budget_email
-  default_tags  = var.default_tags
-}
-
-module "forecast_lambda" {
-  source       = "./modules/forecast_lambda"
-  default_tags = var.default_tags
-}
-
-module "cloudwatch_trigger" {
-  source               = "./modules/cloudwatch_trigger"
-  lambda_function_name = module.forecast_lambda.lambda_name
-  lambda_function_arn  = module.forecast_lambda.lambda_arn
-}
-
-module "cost_report" {
-  source       = "./modules/cost_report"
-  bucket_name  = module.s3_cost_reports.bucket_name
-  default_tags = var.default_tags
-}
